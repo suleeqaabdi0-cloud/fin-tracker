7db665777f2104c367d88964a9a888e2914a537c
suleeqaabdi0-cloude <suleeqaabdi0@gmail.com>
2026-09-16 21:42:52 +0300

    fix github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path"
+import path from "path";
 import { fileURLToPath } from "url";
 
-
-// Maadaama aan isticmaaleyno ESM, waa inaan u dhisnaa __dirname sidan:
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  base: "/fin-tracker/",
+
   server: {
     proxy: {
-      "/api": "http://localhost:5000", // Halkan geli Port-ka uu backend-kaagu ku shaqaynayo
+      "/api": "http://localhost:5000",
     },
   },
+
   plugins: [react(), tailwindcss()],
 
   resolve: {
